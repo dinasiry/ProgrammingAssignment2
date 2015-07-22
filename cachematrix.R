3ae3452f0a3b755969faa84f42f66c02cb61f5dd
Dina <dinasiry@gmail.com>
2015-07-22 13:32:25 -0700

    Edit comment for CacheSolve fxn
    added comment for CacheSolve function

--- cachematrix.R
+++ cachematrix.R
@@ -28,7 +28,8 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
+## The function cacheSolve will invert the matrix and cache the
+## newly inverted matrix.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
